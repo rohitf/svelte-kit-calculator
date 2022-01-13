059547089a20d7844c1af4202f409534194d0ade
Rohit Falor <rohitfalor@outlook.com>
2022-01-13 14:04:32 -0800

    svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
 import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
+	preprocess: [preprocess({})],
 	kit: {
 		adapter: adapter(),
 		target: '#svelte'
 	}
 };
+
